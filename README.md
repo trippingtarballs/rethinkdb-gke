5ae40728a52c583394dc8b336438801122fe1752
Rajiv Bose <rbose85@users.noreply.github.com>
2015-07-16 20:35:53 +0100

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # rethinkdb-gke
-Build a rethinkdb Docker image suitable for running a rethinkdb cluster on Google Container Engine.
+
+This project is to build a simple rethinkdb Docker image suitable for running a rethinkdb cluster on [Google Container Engine](https://cloud.google.com/container-engine/) with [Kubernetes](http://kubernetes.io/).
+
+At present the resulting Docker image can be found at [rbose85/rethinkdb-gke](https://registry.hub.docker.com/u/rbose85/rethinkdb-gke/).
+
+The project is made up of two parts;
+
+ * `docker-image` – from which the docker image is created.
+ * `configuration` – all things pertaining to kubernetes.
+
+`docker-image/Dockerfile` heeds the suggested `Dockerfile` [best practices](https://docs.docker.com/articles/dockerfile_best-practices/#example) in implementing an `entrypoint`.
+
+## How To Use
+
+Launch each file in numerical order in `configuration`. Use the `kubectl create -f <file>.json` command to do so.
---
+++ docker-image/docker-entrypoint.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+set -eo pipefail
+
+export RETHINKDB_JOIN_ARGS
+
+url="https://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_SERVICE_PORT}/api/v1/namespaces/${POD_NAMESPACE:-default}/endpoints"
+cert="/var/run/secrets/kubernetes.io/serviceaccount/ca.crt"
+header="Authorization: Bearer $(cat /var/run/secrets/kubernetes.io/serviceaccount/token)"
+pod_ip=$(ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | cut -f1  -d'/')
+
+setArgs () {
+    if [[ -n "${KUBERNETES_SERVICE_HOST}" ]]; then
+        service="${1}"
+
+        echo "Checking running ${service} ..."
+
+        echo "curl: $(curl -s "${url}/${service}" --cacert "${cert}" --header "${header}")"
+        RETHINKDB_JOIN_ARGS=($(curl -s "${url}/${service}" --cacert "${cert}" --header "${header}" | jq -s -r --arg h "${pod_ip}" '.[0].subsets | .[].addresses | "--join " + .[].ip + ":29015"'))
+
+        echo "RETHINKDB_JOIN_ARGS:" "${RETHINKDB_JOIN_ARGS[@]}"
+    fi
+}
+
+if [ "$1" = 'rethinkdb' ]; then
+    if [[ "${RETHINKDB_INSTANCE_ROLE}" = 'PROXY' ]]; then
+        echo "Adding a PROXY node."
+
+        # find all db-nodes and use their IPs with proxy command to rethinkdb
+        setArgs "db-nodes"
+
+        echo rethinkdb proxy "${RETHINKDB_JOIN_ARGS[@]}" --bind all
+        exec rethinkdb proxy "${RETHINKDB_JOIN_ARGS[@]}" --bind all
+    fi
+
+    if [[ "${RETHINKDB_INSTANCE_ROLE}" = 'NODE' ]]; then
+        echo "Adding a NODE node."
+
+        # find all db-proxies –> either first time node, or cluster growing
+        setArgs "db-proxies"
+
+        if [ ${#RETHINKDB_INSTANCE_ROLE[@]} -eq 0 ]; then
+            exec rethinkdb --bind all
+        fi
+        echo "inside .. RETHINKDB_JOIN_ARGS:" "${RETHINKDB_JOIN_ARGS[@]}"
+        exec rethinkdb "${RETHINKDB_JOIN_ARGS[@]}" --bind all
+
+
+    fi
+fi
+
+exec "$@"
